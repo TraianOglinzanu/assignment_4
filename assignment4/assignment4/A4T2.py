4ee892beb694a3b6f3e1176930b4f6e2878eb208
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2021-11-29 18:42:42 -0700

    Progress

---
+++ assignment4/assignment4/A4T2.py
@@ -0,0 +1,48 @@
+#A4T2.py
+
+from pymongo import MongoClient
+import json
+from bson.json_util import loads
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+
+#create or open database on server 
+db = client["A4dbEmbed"]
+
+# List collection names.
+collist = db.list_collection_names()
+
+if "artists" in collist:
+    print("The collection exists.")
+
+if "tracks" in collist:
+    print("The collection exists.")
+
+# Create or open the collection in the db
+artists_collection = db["artists"]
+tracks_collection = db["tracks"]
+
+f = open('artists.json')
+g = open('tracks.json')
+
+data = loads(f.read())
+data_2 = loads(g.read())
+
+artists_collection.insert_many(data)
+tracks_collection.insert_many(data_2)
+
+ArtistsTracks = db['ArtistsTracks']
+
+pipeline = [{
+    '$lookup':{
+        'from':'tracks',
+        'localField': 'tracks',
+        'foreignField' : 'track_id',
+        'as': 'tracks'
+    }
+}]
+
+for i in artists_collection.aggregate(pipeline):
+    ArtistsTracks.insert_one(i)
+
+
