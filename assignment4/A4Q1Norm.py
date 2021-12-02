68c7b9427078e5a7f32a144af2e2003ec349dbdd
Yalmaz Abdullah <42461443+YalmazAbdullah@users.noreply.github.com>
2021-12-01 21:39:41 -0700

    Q1 and 2 for Norm
    barebones implement,
    formatting todo later

---
+++ assignment4/A4Q1Norm.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import json
+from bson.json_util import loads
+import pprint
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+
+#create or open database on server 
+db = client["A4DBNorm"]
+
+artists_collection = db["artists"]
+
+#important below!!!!
+#right now it should return the ones that have no tracks. inverting the condition will give the right answer but thats harder to check so this is for testing
+for artist in artists_collection.find({"artists": "tracks"}).count()<1:
+  pprint.pprint(artist)#ill figure out the correct formatting later
+
---
+++ assignment4/A4Q2Norm.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import json
+from bson.json_util import loads
+import pprint
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+
+#create or open database on server 
+db = client["A4DBNorm"]
+
+tracks_collection = db["tracks"]
+
+for tracks in tracks_collection.find({"track_id": "/70/"}):#<<should work or fail horribly not sure
+  pprint.pprint(tracks)#ill figure out the correct formatting later
+
